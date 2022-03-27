d08e0a89c0f104ecb17e50eb24f45a8d19b05f30
Viking Zhang <vikingmute@gmail.com>
2022-03-27 22:59:33 +0800

    publish first version

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -3,15 +3,17 @@ import * as path from 'path'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import dts from 'vite-plugin-dts'
+import VueTypeImports from 'vite-plugin-vue-type-imports'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin(), dts()],
+  plugins: [vue(), eslintPlugin(), VueTypeImports(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'VueEasyMDE',
       fileName: (format) => `vue-easymde.${format}.js`
     },
+    emptyOutDir: true,
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
